4cd000a8fe66eb5d0861f0a8ef218c2604b825d4
Mashopy <eliasgheeraert@gmail.com>
2024-04-20 23:22:54 +0200

    modules: oplus: Symlink task_cpustats and gloom .c and .h files to their 5.10 counterparts
    Their oplus_performance_5.10 counterpars doesn't have modules build rule in Makefile, hence the symlinks.

--- vendor/oplus/kernel/mm/gloom/va_feature_node.h
+++ vendor/oplus/kernel/mm/gloom/va_feature_node.h
@@ -1,7 +1,1 @@
-#ifndef _VA_FEATURE_NODE_H_
-#define _VA_FEATURE_NODE_H_
-
-extern int oplus_gloom_proc_init(void);
-extern void oplus_gloom_proc_deinit(void);
-
-#endif /* _VA_FEATURE_NODE_H_  */
+../../oplus_performance_5.10/mm/gloom/va_feature_node.h
